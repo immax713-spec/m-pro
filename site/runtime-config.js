e449ce5182a02bf4bb8de5f10d86fade413b93b6
immax713-spec <immax713@gamil.com>
2026-04-17 19:44:30 +0300

    chore: bump site asset version

--- site/runtime-config.js
+++ site/runtime-config.js
@@ -10,7 +10,7 @@
 
   const deployConfig = {
     ...currentDeployConfig,
-    assetVersion: '20260417c',
+    assetVersion: '20260417d',
     supabaseUrl: 'https://vsdfxrnkxohsuewyvpih.supabase.co',
     supabaseAnonKey: 'sb_publishable_7deVw5OluDBmYalvdq_13Q_yKrCDT5H'
   };
