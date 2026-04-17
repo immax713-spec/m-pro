d47c5b6740a2a5de6f749539606ba6913a155671
immax713-spec <immax713@gamil.com>
2026-04-17 23:34:06 +0300

    fix: rename selection button to add to map

--- site/runtime-config.js
+++ site/runtime-config.js
@@ -10,7 +10,7 @@
 
   const deployConfig = {
     ...currentDeployConfig,
-    assetVersion: '20260417j',
+    assetVersion: '20260417k',
     supabaseUrl: 'https://vsdfxrnkxohsuewyvpih.supabase.co',
     supabaseAnonKey: 'sb_publishable_7deVw5OluDBmYalvdq_13Q_yKrCDT5H'
   };
